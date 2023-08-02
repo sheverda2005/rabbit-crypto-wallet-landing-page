26e65fe245f6f297420fe90bf552451a2a62853c
Sheverda <nsewerda04@gmail.com>
2023-08-02 17:36:18 +0300

    burger menu

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const burger = document.querySelector('.burger')
-
+const burger_menu = document.querySelector(".burger_menu")
+const body = document.querySelector("body")
 burger.addEventListener("click", ()=> {
     burger.classList.toggle("active")
+    burger_menu.classList.toggle("active")
+    body.classList.toggle("burger_active")
 })
